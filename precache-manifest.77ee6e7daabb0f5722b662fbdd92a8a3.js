caffcedc7b60d7650c12ae5ba0f685285379d400
huongnguyen <huonghuonglacey1020@gmail.com>
2020-05-22 18:26:59 -0700

    Updates

---
+++ precache-manifest.77ee6e7daabb0f5722b662fbdd92a8a3.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c760d43251a189639494ebb89a498eaf",
+    "url": "/aboutMe/index.html"
+  },
+  {
+    "revision": "69ceb3c00ebf2b822e3d",
+    "url": "/aboutMe/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "3e76ff255579b24d7304",
+    "url": "/aboutMe/static/css/main.e8eae217.chunk.css"
+  },
+  {
+    "revision": "69ceb3c00ebf2b822e3d",
+    "url": "/aboutMe/static/js/2.845bf1c1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/aboutMe/static/js/2.845bf1c1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3e76ff255579b24d7304",
+    "url": "/aboutMe/static/js/main.bad60973.chunk.js"
+  },
+  {
+    "revision": "56cb29306a3b8350f404",
+    "url": "/aboutMe/static/js/runtime-main.bba41d47.js"
+  },
+  {
+    "revision": "ec250e4475dda0fc23d29e4809849e1d",
+    "url": "/aboutMe/static/media/background.ec250e44.jpg"
+  },
+  {
+    "revision": "609f6f4b5ee207f1d9b1cebef2f73fab",
+    "url": "/aboutMe/static/media/burgerImage.609f6f4b.jpg"
+  },
+  {
+    "revision": "07c6db762204fdf477fd0407f862eb8e",
+    "url": "/aboutMe/static/media/hw10.07c6db76.jpg"
+  },
+  {
+    "revision": "131d3fcd0a5e2f8939d1d6c4e3420b57",
+    "url": "/aboutMe/static/media/noteImage.131d3fcd.jpg"
+  },
+  {
+    "revision": "46a0ed096fd0b6548bd87c817f24f7d8",
+    "url": "/aboutMe/static/media/profile.46a0ed09.jpg"
+  },
+  {
+    "revision": "d886d44d847a7347da9880e77b388d8a",
+    "url": "/aboutMe/static/media/pwImage.d886d44d.jpg"
+  },
+  {
+    "revision": "142f1e3f558769b0edcb107308299fc1",
+    "url": "/aboutMe/static/media/resume.142f1e3f.jpg"
+  },
+  {
+    "revision": "72f86050923e7c3b577a86c2c9451cdc",
+    "url": "/aboutMe/static/media/trackerImage.72f86050.jpg"
+  },
+  {
+    "revision": "2d0bdc0da223bd65e2d8bc452ce17b72",
+    "url": "/aboutMe/static/media/weatherImage.2d0bdc0d.jpg"
+  }
+]);
